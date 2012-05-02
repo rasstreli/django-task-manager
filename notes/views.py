2cd12e3255c96c1b44d143b7e2b2df92c7d0b7f4
pavel <pavel@pavel-H61M-DS2.(none)>
2012-05-02 18:11:49 +0300

    small refactoring

--- notes/views.py
+++ notes/views.py
@@ -13,22 +13,21 @@ def create_job(request):
             return HttpResponseRedirect('/')
     else:
         form = JobForm
-    new_dict = {'form':form}
-    return render_to_response(template,new_dict, context_instance=RequestContext(request))
+    dict_to_return = {'form':form}
+    return render_to_response(template,dict_to_return,context_instance=RequestContext(request))
 
 def job(request, job_id):
     if job_id:
-        list = Jobs.objects.filter(id = job_id)
+        job = Jobs.objects.filter(id = job_id)
     else:
-        list = ''
+        job = ''
     template = "jobs/job.html"
-    jobs_list = Jobs.objects.all()
     form = UpdateJobForm()
-    testdict = {'jobs':jobs_list,'job':list, 'form':form}
-    return render_to_response(template, testdict, context_instance=RequestContext(request))
+    dict_to_return = {'job':job, 'form':form}
+    return render_to_response(template, dict_to_return, context_instance=RequestContext(request))
 
 def job_list(request):
     template = "jobs/job_list.html"
     jobs_list = Jobs.objects.all()
-    test_dict = {'jobs':jobs_list,'form':form}
-    return render_to_response(template, test_dict, context_instance=RequestContext(request))
+    dict_to_return = {'jobs':jobs_list}
+    return render_to_response(template, dict_to_return, context_instance=RequestContext(request))
